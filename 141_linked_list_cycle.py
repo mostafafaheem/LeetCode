2d4138af978fd1d01ce27886a7ebf21cff27d840
Mostafa <125207768+mostafafaheem@users.noreply.github.com>
2025-12-12 10:12:27 +0200

    Code style improvements

--- 141_linked_list_cycle.py
+++ 141_linked_list_cycle.py
@@ -7,11 +7,14 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
             return False
+       
         fast = head
+        
         while fast and fast.next:
             fast = fast.next.next
-
             head = head.next
+            
             if fast == head:
                 return True
+ 
         return False
