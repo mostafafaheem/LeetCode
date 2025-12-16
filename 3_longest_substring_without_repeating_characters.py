d2045a456f2b636aa9c536fed1dbb1c7916f4e32
Mostafa <125207768+mostafafaheem@users.noreply.github.com>
2025-12-16 17:30:53 +0200

    Add 3_longest_substring_without_repeating_characters.py - LeetCode Problem 3

---
+++ 3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s)<3:
+            return len(set(s))
+        mpp = set()
+        p1, p2,maxlen=0,0,0
+        while p2 < len(s) and p1<=p2:
+            while s[p2] in mpp:
+                mpp.remove(s[p1])
+                p1+=1
+            mpp.add(s[p2])
+            maxlen=max(maxlen,p2-p1+1)
+            p2+=1
+        return maxlen
